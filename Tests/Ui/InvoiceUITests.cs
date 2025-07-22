66cfaf000b3a653c622d8b9191b937dc25bf6d1d
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-22 22:41:53 +0200

    Add automation IDs and UI test scaffolding

---
+++ Tests/Ui/InvoiceUITests.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace Facturon.Tests.Ui
+{
+    [TestClass]
+    public class InvoiceUITests
+    {
+        private const string WinAppDriverUrl = "http://127.0.0.1:4723";
+        private WindowsDriver<WindowsElement>? _session;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new AppiumOptions();
+            options.AddAdditionalCapability("app", "Facturon.App.exe");
+            _session = new WindowsDriver<WindowsElement>(new Uri(WinAppDriverUrl), options);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _session?.Quit();
+        }
+
+        [TestMethod]
+        public void TogglePricesIncludeVat_RecalculatesTotals()
+        {
+            Assert.IsNotNull(_session);
+            var numberBox = _session.FindElementByAccessibilityId("InvoiceNumberBox");
+            var issuerBox = _session.FindElementByAccessibilityId("IssuerBox");
+            var netTotalText = _session.FindElementByAccessibilityId("NetTotalText");
+
+            var initialNumber = numberBox.Text;
+            var initialIssuer = issuerBox.Text;
+            var initialNet = netTotalText.Text;
+
+            var vatCheck = _session.FindElementByAccessibilityId("IsGrossBasedCheckBox");
+            vatCheck.Click();
+
+            var newNet = _session.FindElementByAccessibilityId("NetTotalText").Text;
+
+            Assert.AreEqual(initialNumber, numberBox.Text);
+            Assert.AreEqual(initialIssuer, issuerBox.Text);
+            Assert.AreNotEqual(initialNet, newNet);
+        }
+    }
+}
