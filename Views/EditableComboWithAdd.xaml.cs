788228b743430c23d4b41dba7b89fb271d65e0b4
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-23 21:45:18 +0200

    [ui_agent] add DisplayMemberPath to EditableComboWithAdd

--- Views/EditableComboWithAdd.xaml.cs
+++ Views/EditableComboWithAdd.xaml.cs
@@ -7,6 +7,15 @@ namespace Facturon.App.Views
 {
     public partial class EditableComboWithAdd : UserControl
     {
+        public static readonly DependencyProperty DisplayMemberPathProperty = DependencyProperty.Register(
+            nameof(DisplayMemberPath), typeof(string), typeof(EditableComboWithAdd), new PropertyMetadata(null));
+
+        public string DisplayMemberPath
+        {
+            get => (string)GetValue(DisplayMemberPathProperty);
+            set => SetValue(DisplayMemberPathProperty, value);
+        }
+
         public EditableComboWithAdd()
         {
             InitializeComponent();
