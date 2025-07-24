655992360639f82990fd60e9cfbaad2d44d17fbc
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-24 02:10:04 +0200

    [ui_agent] Add window loaded and exit commands

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
 using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
-using Facturon.Services;
 using Facturon.App;
 
 namespace Facturon.App.Views
@@ -10,26 +8,6 @@ namespace Facturon.App.Views
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += Window_Loaded;
-            PreviewKeyDown += Window_PreviewKeyDown;
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            var nav = ((App)Application.Current).Host?.Services.GetRequiredService<INavigationService>();
-            nav?.MoveFocus(FocusNavigationDirection.First);
-        }
-
-        private void Window_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == System.Windows.Input.Key.Escape)
-            {
-                e.Handled = true;
-                var dlg = new ConfirmExitWindow { Owner = this };
-                var result = dlg.ShowDialog();
-                if (result == true)
-                    Close();
-            }
         }
     }
 }
