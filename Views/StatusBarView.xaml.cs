5c00c96763dfe2b2d834878d157808d23f6178a1
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-25 10:03:42 +0200

    ui: add status bar

---
+++ Views/StatusBarView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Facturon.App.Views
+{
+    public partial class StatusBarView : UserControl
+    {
+        public StatusBarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
